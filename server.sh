3578a614dbddf7d3d34daee3ca252fca35bec97d
Daniel Gladstein <daniel.gladstein@neato.com>
2025-09-15 14:52:00 -0700

    trying another new server.sh change

--- server.sh
+++ server.sh
@@ -1,10 +1,8 @@
-# entrypoint.sh
 #!/usr/bin/env bash
-set -euo pipefail
-: "${PORT:=8080}"
-: "${POSTGRESQL_URL:?missing}"
-: "${STORAGE_URL:?missing}"
-exec mlflow server \
-  --host 0.0.0.0 --port "$PORT" \
-  --backend-store-uri "$POSTGRESQL_URL" \
-  --default-artifact-root "$STORAGE_URL"
+
+mlflow db upgrade $POSTGRESQL_URL
+mlflow server \
+  --host 0.0.0.0 \
+  --port 8080 \
+  --backend-store-uri $POSTGRESQL_URL \
+  --artifacts-destination $STORAGE_URL
