3894a60b22c37c2e8c9e98322ca8d9e45e071764
Daniel Gladstein <daniel.gladstein@neato.com>
2025-09-15 16:03:09 -0700

    fixed the secret posting?

--- server.sh
+++ server.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+
+export MLFLOW_TRACKING_USERNAME=$MLFLOW_USERNAME
+export MLFLOW_TRACKING_PASSWORD=$MLFLOW_PASSWORD
 mlflow db upgrade $POSTGRESQL_URL
 mlflow server \
   --host 0.0.0.0 \
   --port 8080 \
   --backend-store-uri $POSTGRESQL_URL \
-  --artifacts-destination $STORAGE_URL \
-  --MLFLOW_TRACKING_USERNAME $MLFLOW_USERNAME \
-  --MLFLOW_TRACKING_PASSWORD $MLFLOW_PASSWORD
+  --artifacts-destination $STORAGE_URL 
